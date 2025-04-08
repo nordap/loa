a1f3576726bd20b34f192e8b4a9ff3d1da02c4fe
kbs <nordap@naver.com>
2025-04-08 10:40:22 +0900

    1

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,12 +1,23 @@
-import { createClient } from '@/utils/supabase/server'
+'use client'
 
-export default async function Page() {
-  const supabase = await createClient()
-  const { data: schedules } = await supabase.from('schedules').select('*')
+import { createClient } from '@/utils/supabase/client'
+import { useEffect, useState } from 'react'
+
+export default function Page() {
+  const [notes, setNotes] = useState<any[] | null>(null)
+  const supabase = createClient()
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await supabase.from('notes').select()
+      setNotes(data)
+    }
+    getData()
+  }, [])
 
   return (
-    <div>
-      {JSON.stringify(schedules)}
-    </div>
+    <pre>
+      {JSON.stringify(notes, null, 2)}
+    </pre>
   )
 }
